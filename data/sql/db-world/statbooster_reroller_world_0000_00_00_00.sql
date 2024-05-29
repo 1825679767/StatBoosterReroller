90ba2ef6b25597394632b6b5004495a886ac8438
1825679767 <124947689+1825679767@users.noreply.github.com>
2024-05-29 15:00:17 +0800

    汉化

--- data/sql/db-world/statbooster_reroller_world_0000_00_00_00.sql
+++ data/sql/db-world/statbooster_reroller_world_0000_00_00_00.sql
@@ -1,1 +1,2 @@
-UPDATE item_template SET name = "Attribute Recalibrator", AllowableClass = -1, ItemLevel = 1, RequiredLevel = 1, RequiredSkill = 0, RequiredSkillRank = 0, spellid_1 = 10694, spelltrigger_1 = 0, description = "|cff00ff00Use: Adds or re-rolls an extra stat on to an item.|n|n|cffff0000WARNING: This will replace any extra stats with a new one." WHERE entry = 41605;
+UPDATE item_template SET name = "属性重置器", AllowableClass = -1, ItemLevel = 1, RequiredLevel = 1, RequiredSkill = 0, RequiredSkillRank = 0, spellid_1 = 10694, spelltrigger_1 = 0, description = "|cff00ff00Use: 为物品添加或重新掷出一个额外属性。|n|n|cffff0000WARNING: 这将会用新的属性替换任何额外的属性。" WHERE entry = 41605;
+UPDATE item_template_locale SET name = '属性重置器' WHERE ID = 41605 AND locale = 'zhCN';
